a06ac1fd695987d3d09025b38889f085dd506a29
shubhajeet2107 <subhajeet.dey@girnarsoft.com>
2016-10-18 16:24:34 +0530

    bot complete

--- xbox_bot.py
+++ xbox_bot.py
@@ -1,6 +1,7 @@
 import praw
+import sendgrid
 import os
-import yagmail
+import datetime, time
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
@@ -9,21 +10,32 @@ username = os.environ.get('REDDIT_USERNAME')
 password = os.environ.get('REDDIT_PASSWORD')
 client_id = os.environ.get('REDDIT_CLIENT_ID')
 client_secret = os.environ.get('REDDIT_CLIENT_SECRET')
-
-gmail_email = os.environ.get('GMAIL_EMAIL')
-gmail_password = os.environ.get('GMAIL_PASS')
+sendgrid_api_key = os.environ.get('SENDGRID_APIKEY')
 
 user_agent = 'XboxOneGiveAways v0.1'
+mail_from = os.environ.get('MAIL_FROM')
 mail_to = os.environ.get('MAIL_TO')
 
-giveaways_keywords = ["give", "away", "giveaway", "give away","free games", "free","for free","givethemall", "giveaways","givesaway"]
+giveaways_keywords = ['[ giveaway ]', 'free games', '[ giveaways ]', 'give', '[giveaway ]', 'away', 'giveaways', 'free', '[giveaway]', 'give away', '[ give away]', 'giveaway', '[ giveaway]', '[giveaways]', 'for free', '[ giveaways ]', '[giveaway]', 'givesaway', 'givethemall', 'giving', 'giving away', 'gives away']
 
+already_sent = []
 
-def send_mail(sub_title):
-	if sub_title:
-		message = "Giveaway on Reddit: "+ str(sub_title)
-		yag = yagmail.SMTP(gmail_email, gmail_password)
-		yag.send(mail_to, 'subject', message)
+def send_mail(submission):
+	client = sendgrid.SendGridClient(sendgrid_api_key)
+	if submission and submission.id not in already_sent:
+		subject = 'Giveaway on Reddit at '+datetime.datetime.now().strftime('%d-%b-%Y %I:%M %p')
+		created_on = time.strftime('%d-%b-%Y %I:%M %p',time.localtime(submission.created_utc))
+		mail_message = "Giveaway on Reddit: <b>"+ str(submission.title) + "</b></br> Click here: <a href='"+submission.url+"'>Reddit link</a> " +"</br>" + submission.selftext + "</br>"+ "Created on: "+str(created_on)
+		
+		message = sendgrid.Mail()
+		message.add_to(mail_to)
+		message.set_from(mail_from)
+		message.set_subject(subject)
+		message.set_html(mail_message)
+
+		client.send(message)
+		print "Email Sent for "+str(submission.title)
+		already_sent.append(submission.id)
 
 
 
@@ -31,8 +43,8 @@ def process_submission(submission):
 	submission_title = submission.title.lower()
 
 	for words in submission_title.split(' '):
-		if words in giveaways_keywords:
-			send_mail(sub_title=submission.title)
+		if words.strip() in giveaways_keywords:
+			send_mail(submission=submission)
 
 def init_bot():
 	bot = praw.Reddit(user_agent=user_agent,
